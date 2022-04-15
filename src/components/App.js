2549c5af55510c67bdf4a61754a2925dbb135edc
ashwrai <ashwini.rai@nokia.com>
2022-04-15 19:50:36 +0530

    added input and output panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 
 import '../App.css';
+import Header from './Header';
+import InputPanel from './InputPanel';
+import OutputPanel from './OutputPanel';
 
 function App() {
   return (
-    <div>Json formatter</div>
+    <>
+      <Header></Header>
+      <div className="ui placeholder segment">
+  <div className="ui two column very relaxed stackable grid">
+    <InputPanel />
+    <OutputPanel />
+  </div>
+  <div className="ui vertical divider">
+    <button className='ui blue submit button' >Convert</button>
+  </div>
+</div>
+
+    </>
   );
 }
 
